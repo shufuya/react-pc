4ed9afd1b82a74987ef7dc2756b40115d4f8044a
shufuya <1357895146@qq.com>
2022-05-27 00:44:21 +0800

    feat: pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from '@/pages/Login';
 import Layout from '@/pages/Layout';
-
+import './App.css';
+import { AuthComponent } from '@/components/AuthComponent'
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='/' element={<Layout />}></Route>
+          <Route path='/' element={<AuthComponent><Layout /></AuthComponent>}></Route>
           <Route path='/Login' element={<Login />}></Route>
         </Routes>
       </div>
